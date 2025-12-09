55fd939813e3f01b6c0a4442be5ea51ac6a8b7e9
Mubson123 <cedricmube@gmail.com>
2025-12-09 01:28:10 +0100

    Init Commit

---
+++ customer-service/src/test/java/com/tontine/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tontine.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
